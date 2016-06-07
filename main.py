cdd7ef17777ce3c40b1c174f7231d05292ac2c8f
keeper121 <keeper121@ya.ru>
2016-06-07 13:19:31 +0300

    add slice?

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ import numpy as np
 #constants
 __need_blurred_image__ = 1          # flag to generate blurred image
 __upscale_coefficient__ = 2         # coefficient for image blurring
-__tile_size__ = 64;
-
+__tile_size__ = 32
+crop_size = 12 # for convolutional
+model_path = "model.ckpt"
 #open directory with images
 def get_directory(folder):
     foundfile = []
@@ -82,13 +83,27 @@ for image in blurred_images:
     image = imread(image)
     blurred_images_tiles.append(image)
 
+
+def centeredCrop(img, new_height, new_width):
+   width =  np.size(img,1)
+   height =  np.size(img,0)
+   left = int(np.ceil((width - new_width)/2.))
+   top = int(np.ceil((height - new_height)/2.))
+   right = int(np.floor((width + new_width)/2.))
+   bottom = int(np.floor((height + new_height)/2.))
+
+   cImg = img[top:bottom, left:right]
+   return cImg
+
+
 # original images tiles
 original_images = get_directory("out/original_tiles")
 original_images_tiles = []
 for image in original_images:
     image = imread(image)
-    original_images_tiles.append(image)
+    original_images_tiles.append(centeredCrop(image, __tile_size__ - crop_size * 2, __tile_size__ - crop_size * 2))
 #--------------------------------------#
+
 
 #--------------------------------------#
 #tensor flow configuration
@@ -99,14 +114,15 @@ f2 = 1
 f3 = 5
 n1 = 64
 n2 = 32
-learning_rate = 0.0001
+learning_rate = 0.001
 training_iters = 100000
-batch_size = 5
+batch_size = 8
 display_step = 10
 n_input = n_output = len(blurred_images)
+
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, __tile_size__, __tile_size__, 3])
-y = tf.placeholder(tf.float32, [None, __tile_size__, __tile_size__, 3])
+y = tf.placeholder(tf.float32, [None, __tile_size__ - crop_size * 2, __tile_size__ - crop_size * 2, 3])
 
 def conv2d(img, w, b):
     return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(img, w, strides=[1, 1, 1, 1], padding='SAME'),b))
@@ -142,47 +158,30 @@ pred = model(x, weights, biases)
 count_batch = n_input / batch_size
 
 # get crop size
-
-crop_size = ((((n1 - n2) / 2.0) - 3.0) / 2.0)
-# crop_size = ((n1 - n2 - 3.0) / 2.0)
-
-#slice_y = y[:, crop_size:__tile_size__ - crop_size, crop_size:__tile_size__ - crop_size, :]
-#slice_pred = pred[:, crop_size:__tile_size__ - crop_size, crop_size:__tile_size__ - crop_size, :]
-
-
 def cost_func(pred, y, crop_size):
-    i = 0
-    sum = tf.Variable(0.0)
-
-    #get center
-    while i < batch_size:
-        slice_y = tf.image.central_crop(y[i,:,:,:], crop_size / __tile_size__)
-        slice_pred = tf.image.central_crop(pred[i,:,:,:], crop_size / __tile_size__)
-        tf.add(sum, tf.reduce_sum(tf.pow(tf.sub(slice_pred, slice_y), 2)))
-        i = i + 1
-
-    return tf.div(sum, (3 * __tile_size__ * __tile_size__ * count_batch * batch_size))
-
+    slice_pred = pred[:, crop_size:__tile_size__ - crop_size, crop_size:__tile_size__ - crop_size, :]
+    return tf.div(tf.reduce_sum(tf.pow(tf.sub(slice_pred, y), 2)), (3 * (__tile_size__ - crop_size) * (__tile_size__ - crop_size) * count_batch * batch_size))
 
 # Define loss and optimizer
 # Euclidean distance
 cost = cost_func(pred, y, crop_size)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 
-# Evaluate model
-correct_pred = tf.equal(pred, y)
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+euclid = cost_func(pred, y, crop_size)
+accuracy = tf.reduce_mean(tf.cast(tf.sqrt(euclid), tf.float32))
 
 # Initializing the variables
 init = tf.initialize_all_variables()
-show_img = 1
+
+# 'Saver' op to save and restore all the variables
+saver = tf.train.Saver()
 
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
     step = 1
     # Keep training until reach max iterations
-    while step < count_batch:
+    while step <= count_batch:
         batch_xs = blurred_images_tiles[batch_size * (step - 1):batch_size * step]
         batch_ys = original_images_tiles[batch_size * (step - 1):batch_size * step]
 
@@ -198,6 +197,10 @@ with tf.Session() as sess:
 
     # Test model
     print "Accuracy:", accuracy.eval({x: blurred_images_tiles[:count_batch * batch_size], y: original_images_tiles[:count_batch * batch_size]})
+
+    # Save model weights to disk
+    save_path = saver.save(sess, model_path)
+    print "Model saved in file: %s" % save_path
 #--------------------------------------#
 
 
