49cd606beed18a9179d127cd4c4b9354ef3d05c7
keeper121 <keeper121@ya.ru>
2016-06-15 12:07:44 +0300

    some changes with crop

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ import skimage.transform
 import tensorflow as tf
 from skimage.io import imsave, imread
 from PIL import Image
-__tile_size__ = 42
+__tile_size__ = 64 #42
 _crop_size = 12 # for convolutional
 
 class SuperResolutionDataSet:
     #constants
     __need_blurred_image__ = 1          # flag to generate blurred image
-    __upscale_coefficient__ = 2         # coefficient for image blurring
+    __upscale_coefficient__ = 3         # coefficient for image blurring
     _original_image_directory = "images"
     _original_tiles_directory = "out/original_tiles"
     _blurred_tiles_directory = "out/blurred_tiles"
 
-    _image_resize_width = 336
-    _image_resize_height = 336
+    _image_resize_width = 256 #336
+    _image_resize_height = 256
 
     #open directory with images
     def get_directory(self, folder):
@@ -157,18 +157,18 @@ class SuperResolutionDataSet:
                     print shape, "->", original_image.shape
             # blurred images tiles
             blurred_images = self.get_directory(self._blurred_tiles_directory)
+            print str(len(blurred_images)) + "x2", "tiles saved"
 
-            print len(blurred_images), "tiles generated"
             for image in blurred_images:
                 image = imread(image)
                 blurred_images_tiles.append(image)
-                # original images tiles
-                original_images = self.get_directory("out/original_tiles")
-
+            print "blurred tiles opened"
+            # original images tiles
+            original_images = self.get_directory(self._original_tiles_directory)
             for image in original_images:
                 image = imread(image)
                 original_images_tiles.append(self.centeredCrop(image, __tile_size__ - _crop_size * 2, __tile_size__ - _crop_size * 2))
-            print str(len(blurred_images)) + "x2", "tiles saved"
+            print "original tiles opened"
 
         return blurred_images_tiles, original_images_tiles
 
@@ -188,16 +188,16 @@ class SuperResolutionModel:
     batch_size = 8
     #n_input = n_output = len(blurred_images)
     # Store layers weight & bias
-    weights = {
-        'wc1': tf.Variable(tf.random_normal([f1, f1, 3, n1]), name="wc1"), # 1 input, n1 outputs
-        'wc2': tf.Variable(tf.random_normal([f2, f2, n1, n2]), name="wc2"), # n1 inputs, n2 outputs
-        'wc3': tf.Variable(tf.random_normal([f3, f3, n2, 3]), name="wc3"), # n2 inputs, 1 outputs
-    }
-    biases = {
-        'bc1': tf.Variable(tf.random_normal([n1]), name="bc1"),
-        'bc2': tf.Variable(tf.random_normal([n2]), name="bc2"),
-        'bc3': tf.Variable(tf.random_normal([3]), name="bc3"),
-    }
+
+    wc1 = tf.Variable(tf.random_normal([f1, f1, 3, n1]), name="wc1") # 1 input, n1 outputs
+    wc2 = tf.Variable(tf.random_normal([f2, f2, n1, n2]), name="wc2") # n1 inputs, n2 outputs
+    wc3 = tf.Variable(tf.random_normal([f3, f3, n2, 3]), name="wc3") # n2 inputs, 1 outputs
+
+    bc1 = tf.Variable(tf.random_normal([n1]), name="bc1")
+    bc2 = tf.Variable(tf.random_normal([n2]), name="bc2")
+    bc3 = tf.Variable(tf.random_normal([3]), name="bc3")
+
+    test = tf.Variable(0.0, name="test")
 
     # tf Graph input
     x = tf.placeholder(tf.float32, [None, __tile_size__, __tile_size__, 3])
@@ -208,37 +208,39 @@ class SuperResolutionModel:
 
     #create model
 
-    def model(self,_X, _weights, _biases):
+    def model(self,_X, wc1, wc2, wc3, bc1, bc2, bc3):
         # Convolution Layer
-        conv1 = self.conv2d(_X, _weights['wc1'], _biases['bc1'])
+        conv1 = self.conv2d(_X, wc1, bc1)
 
         # Convolution Layer
-        conv2 = self.conv2d(conv1, _weights['wc2'], _biases['bc2'])
+        conv2 = self.conv2d(conv1, wc2, bc2)
 
         # Convolution Layer
-        conv3 = self.conv2d(conv2, _weights['wc3'], _biases['bc3'])
+        conv3 = self.conv2d(conv2, wc3, bc3)
         return conv3
 
     # euclid distance
     def cost_func(self, pred, y, crop_size, count_batch):
         slice_pred = pred[:, crop_size:__tile_size__ - crop_size, crop_size:__tile_size__ - crop_size, :]
-        return tf.div(tf.reduce_sum(tf.pow(tf.sub(slice_pred, y), 2)), (3 * (__tile_size__ - crop_size) * (__tile_size__ - crop_size) * count_batch * self.batch_size))
+        return tf.div(tf.reduce_sum(tf.pow(tf.sub(slice_pred, y), 2)), (3 * (__tile_size__ - crop_size) * (__tile_size__ - crop_size) * self.batch_size))
 
 
     def train(self, blurred_images_tiles, original_images_tiles, model_save_name):
         n_input = len(blurred_images_tiles)
         # Construct model
-        with tf.name_scope("model") as scope:
-            pred = self.model(self.x, self.weights, self.biases)
         count_batch = n_input / self.batch_size
-        # Define loss and optimizer
+        with tf.name_scope('model'):
+            pred = self.model(self.x, self.wc1, self.wc2, self.wc3, self.bc1, self.bc2, self.bc3)
+            # Define loss and optimizer
 
-        # Euclidean distance
-        cost = self.cost_func(pred, self.y, _crop_size, count_batch)
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(cost)
+            # Euclidean distance
+            with tf.name_scope('cost'):
+                cost = self.cost_func(pred, self.y, _crop_size, count_batch)
+                optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(cost)
 
-        euclid = self.cost_func(pred, self.y, _crop_size, count_batch)
-        accuracy = tf.reduce_mean(tf.cast(tf.sqrt(euclid), tf.float32))
+            with tf.name_scope('accuracy'):
+                euclid = self.cost_func(pred, self.y, _crop_size, count_batch)
+                accuracy = tf.reduce_mean(tf.cast(tf.sqrt(euclid), tf.float32))
 
         # Initializing the variables
         init = tf.initialize_all_variables()
@@ -249,6 +251,8 @@ class SuperResolutionModel:
         # Launch the graph
         with tf.Session() as sess:
             sess.run(init)
+            writer = tf.train.SummaryWriter("/tmp/log", sess.graph)
+
             step = 1
             # Keep training until reach max iterations
             while step <= count_batch:
@@ -260,6 +264,9 @@ class SuperResolutionModel:
                 acc = sess.run(accuracy, feed_dict={self.x: batch_xs, self.y: batch_ys})
                 # Calculate batch loss
                 loss = sess.run(cost, feed_dict={self.x: batch_xs, self.y: batch_ys})
+
+                model = sess.run(pred, feed_dict={self.x: batch_xs})
+                print model
                 print "Iter " + str(step) + ", Training Accuracy = " + str(acc) + ", loss = " + str(loss)
                 step += 1
 
@@ -268,11 +275,14 @@ class SuperResolutionModel:
             # Test model
             print "Accuracy:", accuracy.eval({self.x: blurred_images_tiles[:self.batch_size], self.y: original_images_tiles[:self.batch_size]})
 
+            print "Last Biases:", self.bc3.eval()
             # Save model weights to disk
             save_path = saver.save(sess, model_save_name)
             print "Model saved in file: %s" % save_path
 
-            writer = tf.train.SummaryWriter("", sess.graph)
+            writer.flush()
+            writer.close()
+
 
     # try get super resolutional from test image
     def evaluate_model(self, model_name, image_path):
@@ -280,16 +290,19 @@ class SuperResolutionModel:
         blurred_image = skimage.transform.resize(imread(image_path), (__tile_size__, __tile_size__));
         blurred_image = [blurred_image]
         saver = tf.train.Saver()
-        val = self.model(self.x, self.weights, self.biases)
+        val = self.model(self.x, self.wc1, self.wc2, self.wc3, self.bc1, self.bc2, self.bc3)
         #y = tf.Variable(tf.random_normal([1, __tile_size__, __tile_size__, 3]))
-        init = tf.initialize_all_variables()
+
         with tf.Session() as sess:
             saver.restore(sess, model_name)
             print("Model restored.")
-            sess.run(init)
-            res = sess.run(val, feed_dict={self.x: blurred_image})
-            img = Image.fromarray(res[0], 'RGB')
+            bc3 = sess.run(self.bc3)
+            print "Restored last Biases:", bc3
 
+            res = sess.run(val, feed_dict={self.x: blurred_image})
+
+            print res
+            img = Image.fromarray(res[0], 'RGB')
             f = pylab.figure()
             #blurred image
             f.add_subplot(2, 1, 1)
